7affaaf56f936744bf4653fc50144793cabd426c
cristian <cristianmedinay@gmail.com>
2023-09-12 15:56:39 +0200

    data provider component

---
+++ src/components/DataProvider.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {ref} from 'vue';
+const props = defineProps({
+    url:String,
+})
+
+const data = ref(null)
+const loading = ref(true)
+const error = ref(null)
+
+fetch(props.url)
+    .then(res=>res.json())
+    .then(json=>{
+        data.value=json;
+    })
+    .catch(e=> error.value=e)
+    .finally(()=>loading.value = false)
+
+
+
+</script>
+
+<template>
+    
+    <slot :data="data" :loading="loading" :error="error"/>
+
+</template>
